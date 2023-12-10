ecbefc597228841925cb1ebdc28a5fd9d5df5375
tan-steven <steventan1380@gmail.com>
2023-12-09 20:33:19 -0500

    added functionalities for the project

---
+++ src/import_csv.sql
@@ -0,0 +1,8 @@
+.mode csv
+.import "D:/db_project/data/doctors.csv" DOCTORS
+.import "D:/db_project/data/instruments.csv" INSTRUMENTS
+.import "D:/db_project/data/n_assists.csv" N_ASSIST
+.import "D:/db_project/data/nurses.csv" NURSES
+.import "D:/db_project/data/p_assignment.csv" P_ASSIGNMENT
+.import "D:/db_project/data/patients.csv" PATIENTS
+.import "D:/db_project/data/tests.csv" TESTS
